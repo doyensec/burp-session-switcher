6165adcadfbbb96c12c724600438fed7644527c0
lokiuox <saviosisco@gmail.com>
2024-07-14 22:52:24 +0200

    Add some settings items

--- src/main/kotlin/sessionswitcher/settings/Settings.kt
+++ src/main/kotlin/sessionswitcher/settings/Settings.kt
@@ -9,5 +9,10 @@ class Settings(val provider: SettingsProvider) {
     public val registerContextMenu = BooleanSetting(provider,"ui.register_context_menu", "Register the context menu handler", true)
     public val displayExtensionMainTab = BooleanSetting(provider,"ui.display_extension_main_tab", "Display the extension's main tab", true)
 
-    public val editorFontSize = IntSetting(provider, "editor.font_size", "Request Editor Font Size", 12)
+    public val editorShowRequestBody = BooleanSetting(provider, "editor.show_request_body", "Show the body in the request editor", true)
+    public val editorShowChangesOnly = BooleanSetting(provider, "editor.show_changes_only", "Only show changed headers", false)
+    public val editorHideCommonHeaders = BooleanSetting(provider, "editor.hide_common_headers", "Hide common headers", false)
+
+    public val showAllSessions = BooleanSetting(provider, "session.show_all_sessions", "Do not filter Sessions by hostname", false)
+    public val keepOtherCookies = BooleanSetting(provider, "session.keep_other_cookies", "Keep unchanged cookies in request", true)
 }
