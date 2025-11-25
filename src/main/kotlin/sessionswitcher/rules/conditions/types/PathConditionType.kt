8d5309b9cdbbe5de0aa48d0fc2922f9d72783086
Savio Sisco <savio@doyensec.com>
2025-11-25 23:31:37 +0100

    Added cookie and path conditions

---
+++ src/main/kotlin/sessionswitcher/rules/conditions/types/PathConditionType.kt
@@ -0,0 +1,13 @@
+package sessionswitcher.rules.conditions.types
+
+import burp.api.montoya.http.message.requests.HttpRequest
+import sessionswitcher.rules.conditions.ConditionConfig
+import sessionswitcher.rules.conditions.MatchInfo
+
+object PathConditionType :
+    StringConditionType(matchOn = "Path", matchesOnResponse = false) {
+    override fun matchesRequest(configuration: ConditionConfig, request: HttpRequest, matchInfo: MatchInfo): Boolean {
+        val url = request.pathWithoutQuery()
+        return this.stringMatches(configuration, url)
+    }
+}
---
+++ src/main/kotlin/sessionswitcher/rules/conditions/types/RequestCookieConditionType.kt
@@ -0,0 +1,23 @@
+package sessionswitcher.rules.conditions.types
+
+import burp.api.montoya.http.message.requests.HttpRequest
+import sessionswitcher.rules.conditions.ConditionConfig
+import sessionswitcher.rules.conditions.MatchInfo
+import sessionswitcher.sessions.Cookies
+
+object RequestCookieConditionType :
+    StringConditionType(matchOn = "Request Cookie", matchesOnResponse = false) {
+    override fun matchesRequest(configuration: ConditionConfig, request: HttpRequest, matchInfo: MatchInfo): Boolean {
+        val cookies = Cookies.fromHttpRequest(request)
+        val cookiePairs = cookies.getPairs().map { "${it.first}=${it.second}" }
+        return if (configuration.negativeMatch) {
+            cookiePairs.none { this.stringMatches(configuration, it, false) }
+        } else {
+            cookiePairs.any { this.stringMatches(configuration, it, false) }
+        }
+    }
+
+    override fun describe(configuration: ConditionConfig): String {
+        return "${if (configuration.negativeMatch) "No" else "Any"} ${this.matchOn} ${configuration.operation.lowercase()} \"${configuration.pattern.get()}\""
+    }
+}
